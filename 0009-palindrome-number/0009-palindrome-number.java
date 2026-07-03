b6ca9b710e0c30af207577b30ca061b008045560
Shubham mehta <70097543+shubhamKmehta@users.noreply.github.com>
2026-07-03 21:43:46 +0530

    Time: 5 ms (79.77%), Space: 46.1 MB (33.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if( x<0 || (x %10 ==0 && x != 0 ))return false;
+
+        int reverseHalf =0;
+
+        while(x > reverseHalf){
+            reverseHalf = reverseHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reverseHalf || x == reverseHalf /10;
+    }
+}
