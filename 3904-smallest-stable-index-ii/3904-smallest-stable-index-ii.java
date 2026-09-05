44b51c9cdb23e7f8d5215a8168d779086da3f33e
Shubham mehta <70097543+shubhamKmehta@users.noreply.github.com>
2026-09-05 22:56:48 +0530

    Time: 3 ms (100%), Space: 133.5 MB (8.01%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int[] minElementArray = new int[n];
+        int minEl = Integer.MAX_VALUE;
+
+        for(int i=n-1;i>=0;i--){
+            minEl = Math.min(minEl,nums[i]);
+            minElementArray[i]=minEl;
+        }
+
+        int maxEl =0;
+
+        for(int i=0;i<n;i++){
+            maxEl = Math.max(maxEl,nums[i]);
+
+            if(maxEl - minElementArray[i] <= k){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
